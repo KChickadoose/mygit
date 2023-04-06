b17060f98e93925023e064646af92c3707e7f871
KChickadoose <119143813+KChickadoose@users.noreply.github.com>
2023-04-06 15:18:01 +0700

    Update mygit.py

--- mygit.py
+++ mygit.py
@@ -7,3 +7,12 @@ import plotly.graph_objects as go
 
 st.header("kit")
 st.image("./pic/ocean-3605547__480.jpg")
+
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
