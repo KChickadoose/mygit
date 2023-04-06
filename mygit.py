c2481794c6bed49ba164e4f0cc1d113739f2ea2a
KChickadoose <119143813+KChickadoose@users.noreply.github.com>
2023-04-06 16:20:14 +0700

    Update mygit.py

--- mygit.py
+++ mygit.py
@@ -62,11 +62,11 @@ if st.button("ทำนายผล"):
    st.write(Knn_model.predict(x_input))
 
    out=Knn_model.predict(x_input)
-
-    if out[0]=="Setosa":
-       st.image("./pic/Irissetosa.jpg")
-    else:
-        st.writ("xxxx")
+    
+    if out[0] == 'Setosa':
+        st.image("./pic/setosa.jpg")
+    else:       
+        st.writ('xxx')   
 
    st.button("ไม่แสดงข้อมูล")
 else:
